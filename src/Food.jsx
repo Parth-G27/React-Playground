f39631c2949ca3f7e8868e7b55cb64c5831f4ca3
Parth <parthbidari13@gmail.com>
2024-03-08 18:59:22 +0530

    website basics

---
+++ src/Food.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Food() {
+
+    const item1 = "Rice";
+    const item2 = "Roti";
+    const item3 = "Idle";
+
+
+  return (
+    <>
+    <h3>Food items available</h3>
+    <ol>
+        <li>{item1}</li>
+        <li>{item2}</li>
+        <li>{item3}</li>
+    </ol>
+    <hr />
+
+    </>
+  )
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer(){
+    return(
+        
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Website Name</p>
+        </footer>
+        
+    )
+}
+
+export default Footer;
